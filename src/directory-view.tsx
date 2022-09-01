0be7f49217fbfe6d3325d841694dde94a89a348a
David Zukowski <david@zuko.me>
2022-08-31 20:42:53 -0700

    Selection: implement basic 'selected' state for FSItem

--- src/directory-view.tsx
+++ src/directory-view.tsx
@@ -1,4 +1,5 @@
 import {useCallback, useMemo} from "react"
+import {computed} from "mobx"
 import {observer} from "mobx-react-lite"
 import {FSItem, FSViewModel} from "./file-system"
 import {Virtualizer} from "./virtualizer"
@@ -8,7 +9,13 @@ export let DirectoryView = ({view}: {view: FSViewModel}) => {
         return (
             <div key={row[0].name} style={{display: "flex"}}>
                 {row.map((item) => {
-                    return <DirectoryViewItem key={item.name} item={item} />
+                    return (
+                        <DirectoryViewItem
+                            key={item.name}
+                            item={item}
+                            view={view}
+                        />
+                    )
                 })}
             </div>
         )
@@ -43,9 +50,22 @@ export let DirectoryView = ({view}: {view: FSViewModel}) => {
 }
 DirectoryView = observer(DirectoryView)
 
-let DirectoryViewItem = ({item}: {item: FSItem}) => {
+let DirectoryViewItem = ({item, view}: {item: FSItem; view: FSViewModel}) => {
+    const selected = computed(() => view.selected(item)).get()
+    console.log({selected})
     return (
-        <div>
+        <div
+            style={{background: selected ? "#aaf" : "#fff"}}
+            onClick={(e) => {
+                if (e.ctrlKey) {
+                    if (selected) {
+                        view.selection.delete(item)
+                    } else {
+                        view.selection.add(item)
+                    }
+                }
+            }}
+        >
             <span>{item.name}</span>
         </div>
     )
--- src/file-system.ts
+++ src/file-system.ts
@@ -51,9 +51,11 @@ export class Directory {
 
 export class FSViewModel {
     cwd: Directory
+    selection: Selection<FSItem>
 
     constructor(dir: Directory) {
         this.cwd = dir
+        this.selection = new Selection()
         makeAutoObservable(this, {
             cwd: observable.ref,
         })
@@ -69,5 +71,49 @@ export class FSViewModel {
         const directory = new Directory(name)
         this.cwd.add(directory)
         return directory
+    }
+
+    selected(item: FSItem) {
+        return this.selection.has(item)
+    }
+}
+
+/**
+ * Selection holds a unique set of T. It's separate from FSViewModel because
+ * it should be possible for the user to create different selections across
+ * views even if they share the same view model. For similar reasons, selection
+ * state is not tracked on the item itself, since that selection state is
+ * contextual (i.e. it may be selected in one view but not another).
+ *
+ * Currently this class isn't much more than a proxy around an observable set,
+ * so it's a bit silly. Since it's not the objective of this exercise, I'm not
+ * sure I'll be able to flesh this out entirely, but in the Real World it
+ * should handle actions such as selection undo/redo, so I'm representing it
+ * accordingly.
+ */
+class Selection<T> {
+    private selection: Set<T>
+
+    constructor() {
+        this.selection = new Set()
+        makeAutoObservable<this, "selection">(this, {
+            selection: observable.shallow,
+        })
+    }
+
+    clear() {
+        this.selection.clear()
+    }
+
+    has(item: T): boolean {
+        return this.selection.has(item)
+    }
+
+    add(item: T) {
+        this.selection.add(item)
+    }
+
+    delete(item: T) {
+        this.selection.delete(item)
     }
 }
