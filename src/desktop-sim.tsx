40ae1f813763f6a39723e0edacee15bac8847617
David Zukowski <david@zuko.me>
2022-08-31 22:23:59 -0700

    Desktop: scale window size based on available space, implement dragging

--- src/desktop-sim.tsx
+++ src/desktop-sim.tsx
@@ -1,7 +1,11 @@
 import "./desktop-sim.css"
-import {createContext} from "react"
+import {createContext, useEffect, useLayoutEffect, useRef} from "react"
 import {makeAutoObservable, observable} from "mobx"
 import {observer} from "mobx-react-lite"
+
+const WINDOW_ASPECT_RATIO = 4 / 3
+const WINDOW_MIN_WIDTH = 480
+const WINDOW_MAX_WIDTH = 768
 
 export let Desktop = ({windows: wm}: {windows: WindowManager}) => {
     return (
@@ -59,6 +63,8 @@ class DesktopWindow {
     }
 }
 
+const WindowContext = createContext<DesktopWindow>(null!)
+
 let Window = ({
     window: win,
     children,
@@ -66,10 +72,13 @@ let Window = ({
     window: DesktopWindow
     children: React.ReactNode
 }) => {
+    const ref = useRef<HTMLDivElement>(null!)
+    const titlebarRef = useRef<HTMLElement>(null!)
+    useWindowBehavior(ref, titlebarRef)
     return (
         <WindowContext.Provider value={win}>
-            <div className="window">
-                <header className="window-titlebar">
+            <div className="window" ref={ref}>
+                <header className="window-titlebar" ref={titlebarRef}>
                     <span className="window-title">{win.title}</span>
                 </header>
                 <div className="window-body">{children}</div>
@@ -79,4 +88,85 @@ let Window = ({
 }
 Window = observer(Window)
 
-const WindowContext = createContext<DesktopWindow>(null!)
+const useWindowBehavior = (
+    ref: React.MutableRefObject<HTMLDivElement>,
+    titlebarRef: React.MutableRefObject<HTMLElement>,
+) => {
+    // size this window based on the available space in the browser window,
+    // preserving intended aspect ratio.
+    // TODO: resize if necessary when the browser window resizes.
+    // TODO: better initial positioning for tiny screens.
+    useLayoutEffect(() => {
+        const elem = ref.current
+        if (!elem) return
+
+        const container = document.body.getBoundingClientRect()
+        const width = clamp(
+            Math.round(container.width * 0.5),
+            WINDOW_MIN_WIDTH,
+            WINDOW_MAX_WIDTH,
+        )
+        const height = width / WINDOW_ASPECT_RATIO
+        const top = container.height / 2 - height / 2
+        const left = container.width / 2 - width / 2
+        elem.style.top = top + "px"
+        elem.style.left = left + "px"
+        elem.style.width = width + "px"
+        elem.style.height = height + "px"
+    }, [ref, titlebarRef])
+
+    // The window can be moved by dragging its titlebar.
+    useDragListener(titlebarRef, (e) => {
+        const elem = ref.current
+        if (!elem) return
+
+        const rect = elem.getBoundingClientRect()
+        const dx = e.movementX
+        const dy = e.movementY
+        elem.style.left = rect.x + dx + "px"
+        elem.style.top = rect.y + dy + "px"
+    })
+}
+
+/**
+ * Reports mouse move events when the user is dragging the target element.
+ * Accepts a callback rather than returning a value to reduce re-renders
+ * in the calling component. Expects the ref's interior value to be stable.
+ */
+export const useDragListener = (
+    ref: React.MutableRefObject<HTMLElement>,
+    onMouseMove: (e: MouseEvent) => void,
+) => {
+    // Avoid thrashing the document with add/remove event listeners just
+    // because the callback changed.
+    const callback = useRef<typeof onMouseMove>(onMouseMove)
+    callback.current = onMouseMove
+    useEffect(() => {
+        const element = ref.current
+        let mousedown = false
+        const handleMouseDown = () => {
+            mousedown = true
+        }
+        const handleMouseUp = () => {
+            mousedown = false
+        }
+        const handleMouseMove = (e: MouseEvent) => {
+            if (mousedown) {
+                e.preventDefault()
+                callback.current(e)
+            }
+        }
+        element.addEventListener("mousedown", handleMouseDown, true)
+        document.addEventListener("mouseup", handleMouseUp, true)
+        document.addEventListener("mousemove", handleMouseMove, true)
+        return () => {
+            element.removeEventListener("mousedown", handleMouseDown)
+            document.removeEventListener("mouseup", handleMouseUp)
+            document.removeEventListener("mousemove", handleMouseMove)
+        }
+    }, [ref])
+}
+
+const clamp = (value: number, min: number, max: number) => {
+    return Math.min(Math.max(value, min), max)
+}
